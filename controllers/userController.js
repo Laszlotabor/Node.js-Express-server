3533d9e4ce97b59b6a6778e4952259a24738ac06
LesButcher <laszlotabor@gmail.com>
2025-05-21 06:01:26 +0200

    5th  saving

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // @desc    Register a user
 // @route   POST /api/users/register
@@ -45,11 +46,39 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-// Placeholder login and current user handlers
+//---------------------------------------------------------------------------------------------------------
+// @desc    login user
+// @route   POST /api/users/login
+// @access  Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Login user" });
+  const { useremail, password } = req.body;
+  if (!useremail || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory!");
+  }
+  const user = await User.findOne({ useremail });
+  //compare password with hashed password
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign({
+      user: {
+        username: user.username,
+        useremail: user.useremail,
+        id: user.id
+      }
+    }, process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "1m" }
+    );
+    res.status(200).json({ accessToken });
+  } else {
+    res.status(401);
+    throw new Error("Email or password not valid!!!!")
+  }
+  
 });
-
+//---------------------------------------------------------------------------------------------------------
+// @desc    current user
+// @route   POST /api/users/current
+// @access  Public
 const currentUser = asyncHandler(async (req, res) => {
   res.json({ message: "Current user info" });
 });
